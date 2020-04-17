b6de552b329c0c0f0ad071bf21b61afb8dd81071
Chris-Bortel <bortelcc19@gmail.com>
2020-04-17 16:11:38 -0700

    hour array

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ function Store ( city, minCustomers, maxCustomers, avgCookieSale){
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookieSale = avgCookieSale;
+
+  this.hours = ['6am', '7am','8am','9am','10am','11am','12pm', '1pm','2pm','3pm', '4pm', '5pm','6pm','7pm' ];
 }
 
 

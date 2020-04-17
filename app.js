c6f06f6be7e0dffe591cb10a7ddd61a8172fba4f
Chris-Bortel <bortelcc19@gmail.com>
2020-04-17 15:59:06 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//TODO:
+
+function Store ( city, minCustomers, maxCustomers, avgCookieSale){
+  this.city = city;
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers;
+  this.avgCookieSale = avgCookieSale;
+}
+
+
+var seattle = new Store('Seattle', 23, 65, 6.3);
+var tokyo = new Store('Tokyo', 3, 24, 1.2);
+var dubai = new Store('Dubai', 11, 38, 3.7);
+var paris = new Store('Paris', 20, 38, 4.6);
+var lima = new Store('Lima', 2, 16, 4.6);
+// console.log(seattle);
+
+console.log(seattle);
+console.log(tokyo);
+console.log(dubai);
+console.log(paris);
+console.log(lima);
+
+
+
+
+
+
+
+
